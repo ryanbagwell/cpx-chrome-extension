5add55b8b5b087b067a956c17c9ef2e60716e7de
Ryan Bagwell <ryan@ryanbagwell.com>
2013-10-11 19:24:40 -0500

    added settings page

--- app/browseraction.js
+++ app/browseraction.js
@@ -1,6 +1,15 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	var tabOptions = {
-		url: 'http://cnpx.hzdesign.com:8080'
+
+    var url = localStorage.cnpURL;
+
+    if (_.isEmpty(url) || _.isUndefined(url) || url == '') {
+        alert('Please set a C&P URL in the options page first.')
+        return;
+    }
+
+    var tabOptions = {
+		url: url
 	};
+
 	var tab = chrome.tabs.create(tabOptions);
 });
---
+++ app/settings.js
@@ -0,0 +1,32 @@
+
+var SettingsView = Backbone.View.extend({
+
+    events: {
+        "click button": "save"
+    },
+
+    el: $('form'),
+
+    initialize: function(options) {
+        _.bindAll(this);
+        this.on('saved', this.showAlert);
+        $('#cnp-url').val(localStorage['cnpURL']);
+    },
+
+    save: function(e) {
+        e.preventDefault();
+        localStorage['cnpURL'] = $('#cnp-url').val();
+        this.trigger('saved');
+    },
+
+    showAlert: function(e) {
+        this.$el.find('.message-wrapper .alert').show().delay(2000).fadeOut(1000);
+    }
+
+});
+
+jQuery(document).ready(function() {
+    var settings = new SettingsView();
+});
+
+
